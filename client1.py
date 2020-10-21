bd8a9ee4a5a511dfbccec257a33282f35b648f73
Riccardo Cherchi <richicherchi@gmail.com>
2020-10-21 16:09:30 +0200

    Initial commit

---
+++ client1.py
@@ -0,0 +1,53 @@
+import json
+import paho.mqtt.client as mqtt
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+
+# The callback for when the client receives a CONNACK response from the server.
+
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    print(client)
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("iot/light")
+    client.subscribe("test")
+
+# The callback for when a PUBLISH message is received from the server.
+
+
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+    json_message = None
+    
+    try:
+        json_message = json.loads(msg.payload)
+    except:
+        print("json error")
+
+    status = json_message['status']
+    
+    print(status)
+    GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)
+    
+    if status:   
+        GPIO.output(8, GPIO.HIGH)
+    else:
+        GPIO.output(8, GPIO.LOW)
+
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("192.168.0.101", 1883, 60)
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()
